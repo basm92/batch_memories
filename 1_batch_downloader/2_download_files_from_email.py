14ed9b0fb08e3bf0236e6c537a1dc98d79743956
Bas Machielsen <basmachielsen@live.nl>
2025-09-17 14:24:33 +0200

    rename stuff

---
+++ 1_batch_downloader/2_download_files_from_email.py
@@ -0,0 +1,8 @@
+# Download emails from gmail
+## Step 1 (through Playwright) Open Browser and go to Gmail (Through Playwright)
+
+## Step 2 Manually Control Playwright and login to the email inbox
+
+
+## Step 3 (through Playwright) Open all archive emails and download the files to the pc
+
